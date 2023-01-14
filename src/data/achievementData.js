ee0e4e5b202ab371eade6d820e1632919c75fd2b
Usama <usamamehar435@gmail.com>
2023-01-14 20:56:25 +0500

    git init

---
+++ src/data/achievementData.js
@@ -0,0 +1,47 @@
+export const achievementData = {
+  bio: "The key is to set achievable goals, work hard and stay committed to them, and celebrate every small success along the way. It will help to stay motivated, and also help to measure the progress and improvement.",
+  achievements: [
+    {
+      id: 1,
+      title: "Cisco CCNA (200-301) Network Fundamentals and Access",
+      details:
+        "knowledge and skills in networking fundamentals such as routing and switching,  IP connectivity, IP services, security basics.",
+      date: "Jan 03, 2022",
+      field: "Networking",
+      image:
+        "https://media.licdn.com/dms/image/C561FAQHp_UjEXYqnKQ/feedshare-document-cover-images_1280/0/1641189922783?e=1674316800&v=beta&t=dK_jF3snQ-OxPxDdBOkoFJAu_T3J6N92kE5AAjg_0Qg",
+    },
+    {
+      id: 2,
+      title: "Graphics Designing",
+      details:
+        "Validate Designing and User Experience skills to grow your career and business.",
+      date: "Sep 2021",
+      field: "Designing",
+      image:
+        "https://i.ibb.co/XJzYcf3/Graphics-Designing-Certificate-Y8-YCAEHPQ-1.jpg",
+    },
+    {
+      id: 3,
+      title: "Java",
+      details:
+        "Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.",
+      date: "Aug 20, 2019",
+      field: "Programing",
+      image:
+        "https://media.licdn.com/dms/image/C561FAQFfWl6JxWic1w/feedshare-document-cover-images_1280/0/1641554377182?e=1674316800&v=beta&t=EQ4ZUzQWZgmD4n4tRc_L22BDbHviS_MDP7TohB8YyNA",
+    },
+  ],
+};
+
+// Do not remove any fields.
+// Leave it blank instead as shown below.
+
+/*
+
+export const achievementData = {
+    bio : "",
+    achievements : []
+}
+
+*/
---
+++ src/data/projectsData.js
@@ -0,0 +1,104 @@
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/five.svg";
+import six from "../assets/svg/projects/six.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import eight from "../assets/svg/projects/eight.svg";
+
+export const projectsData = [
+  {
+    id: 1,
+    projectName: "Orecto Online Shopping",
+    projectDesc:
+      "An Online E-commerece Marketplace that brings you the most popular branded merchandise comprising of the latest and most advanced levels of products.",
+    tags: ["React", "Node", "Express"],
+    code: "",
+    demo: "https://github.com/Usama-Akbar",
+    image: five,
+  },
+  {
+    id: 2,
+    projectName: "Mobala FX",
+    projectDesc:
+      "This project is about a money trasnfer web app. Its provides us the ability to exchange money internationly through people not from the banks.",
+    tags: ["React", "Firebase", "Bootstrap"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://honeymoney.hassancomputing.com/",
+    image: three,
+  },
+  {
+    id: 3,
+    projectName: "Helpify 24",
+    projectDesc:
+      "A Ticket Support Shopify App that allows the merchants to manage all the clients issues and report them.",
+    tags: ["Node", "Zendesk", "Shopify CLI"],
+    code: "https://gitlab.com/qfnetwork/shopify-zendesk-tickets",
+    demo: "https://zendesk.helpify24.com/",
+    image: two,
+  },
+  {
+    id: 4,
+    projectName: "Sawda",
+    projectDesc:
+      "An Online Retailer Marketplace that brings you the most popular branded merchandise comprising of the latest and most advanced levels of products.",
+    tags: ["HTML", "CSS", "JS"],
+    code: "",
+    demo: "https://sawda.com.pk/",
+    image: four,
+  },
+  {
+    id: 5,
+    projectName: "Double Horse",
+    projectDesc:
+      "A Lubricants E-commerce application.The Company's mission is to be a leader in bringing innovative technology to the lubricant category in Pakistan.",
+    tags: ["Wordpress"],
+    code: "",
+    demo: "https://doublehorse.com.pk/",
+    image: six,
+  },
+  //   {
+  //     id: 6,
+  //     projectName: "Uber Lite",
+  //     projectDesc: "Uber clone",
+  //     tags: ["Flutter"],
+  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     image: one,
+  //   },
+  //   {
+  //     id: 7,
+  //     projectName: "Stock Market App",
+  //     projectDesc: "A simple stock market API app",
+  //     tags: ["React", "Redux", "Bootstrap"],
+  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     image: seven,
+  //   },
+  //   {
+  //     id: 8,
+  //     projectName: "Car Pooling System",
+  //     projectDesc:
+  //       "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
+  //     tags: ["Flutter", "React"],
+  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+  //     image: eight,
+  //   },
+];
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
